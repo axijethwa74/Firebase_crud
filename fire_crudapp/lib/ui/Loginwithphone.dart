91a027a46cffc3205f91ffc279f8ea527bcbc612
Yougesh Jethwa <105840649+axijethwa74@users.noreply.github.com>
2023-02-07 12:54:01 +0500

    done

--- fire_crudapp/lib/ui/Loginwithphone.dart
+++ fire_crudapp/lib/ui/Loginwithphone.dart
@@ -1,3 +1,8 @@
+import 'package:fire_crudapp/ui/verifycode.dart';
+import 'package:fire_crudapp/utils/utils.dart';
+import 'package:fire_crudapp/widgets/RoundButton.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class LoginWithPhonenumber extends StatefulWidget {
@@ -9,23 +14,68 @@ class LoginWithPhonenumber extends StatefulWidget {
 
 class _LoginWithPhonenumberState extends State<LoginWithPhonenumber> {
   final phonenumber = TextEditingController();
+  bool Loading = false;
+
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: const Text("Login"),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
+        padding: const EdgeInsets.symmetric(horizontal: 50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
               controller: phonenumber,
-              decoration: InputDecoration(
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
                 hintText: "+1 234 5678 98",
               ),
             ),
+            const SizedBox(
+              height: 50,
+            ),
+            RoundButton(
+                loading: Loading,
+                title: "Login",
+                onTap: () {
+                  // setState(() {
+                  //   Loading = true;
+                  // });
+                  auth.verifyPhoneNumber(
+                      phoneNumber: phonenumber.text.toString(),
+                      verificationCompleted: (_) {
+                        // setState(() {
+                        //   Loading = false;
+                        // });
+                      },
+                      verificationFailed: (e) {
+                        // setState(() {
+                        //   Loading = false;
+                        // });
+                        Utils().toastMessage(e.toString());
+                      },
+                      codeSent: (String verificationid, int? token) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  VerifyCode(verficationId: verificationid),
+                            ));
+                        // setState(() {
+                        //   Loading = false;
+                        // });
+                      },
+                      codeAutoRetrievalTimeout: (e) {
+                        Utils().toastMessage(e.toString());
+                        // setState(() {
+                        //   Loading = false;
+                        // });
+                      });
+                })
           ],
         ),
       ),
--- fire_crudapp/lib/ui/verifycode.dart
+++ fire_crudapp/lib/ui/verifycode.dart
@@ -1,7 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:fire_crudapp/ui/PostScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'package:fire_crudapp/widgets/RoundButton.dart';
+
+import '../utils/utils.dart';
+
 class VerifyCode extends StatefulWidget {
-  const VerifyCode({super.key});
+  final String verficationId;
+  const VerifyCode({
+    Key? key,
+    required this.verficationId,
+  }) : super(key: key);
 
   @override
   State<VerifyCode> createState() => _VerifyCodeState();
@@ -10,6 +21,10 @@ class VerifyCode extends StatefulWidget {
 final phonenumberController = TextEditingController();
 
 class _VerifyCodeState extends State<VerifyCode> {
+  final verifycode = TextEditingController();
+
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +32,35 @@ class _VerifyCodeState extends State<VerifyCode> {
           title: Text("Verify Code"),
         ),
         body: Column(
-          children: [],
+          children: [
+            TextFormField(
+              controller: verifycode,
+              decoration: InputDecoration(
+                hintText: "Code",
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            RoundButton(
+                title: "Verify your Code",
+                onTap: () async {
+                  // ignore: unused_local_variable
+                  final credential = PhoneAuthProvider.credential(
+                    verificationId: widget.verficationId,
+                    smsCode: verifycode.text.toString(),
+                  );
+                  try {
+                    await auth.signInWithCredential(credential);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: ((context) => PostScreen())));
+                  } catch (e) {
+                    Utils().toastMessage(e.toString());
+                  }
+                }),
+          ],
         ));
   }
 }
